5147e9030b87c7243734cb90ddeaa18ea485e8f2
jjhw0106 <jjhw0106@jinhak.com>
2023-10-18 17:24:44 +0900

    Feat: vue cli 설정 및 파일 전송 구조로 코드 변경

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <app-header 
+    v-bind:propsdata="str"
+    v-on:renew="renewStr"
+    >
+    </app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    'app-header': AppHeader
+  },
+
+  data: function() {
+    return {
+      str:'DeadLine'
+    }
+  },
+
+  methods: {
+    renewStr: function() {
+      this.str = 'new DeadLine';
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ propsdata }} 입니다.</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      headLine:"머릿말"
+    }
+  },
+  props: ['propsdata'],
+  methods: {
+    sendEvent: function() {
+      this.$emit('renew')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
