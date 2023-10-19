7914c4e062973567bdd8284a79934bd18798ecb5
jjhw0106 <jjhw0106@jinhak.com>
2023-10-19 14:30:56 +0900

    Feat Vue 기초 완강

---
+++ vue-form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- submit이후 화면이 새로고침 되는 것을 막기위해 .prevent 붙인다. -->
+  <form v-on:submit.prevent ="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">password: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/posts';
+      var data = {
+        username: this.username,
+        password: this.password,
+      }
+      axios
+        .post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
